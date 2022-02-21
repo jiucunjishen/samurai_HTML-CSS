e1f8a9b3acfb45308ae3b1210bb45e031c219172
jiucun <yoshinobu.hisamura@gmail.com>
2022-02-21 20:06:32 +0900

    20220221

---
+++ ProgressManagementSystem/Case.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressManagementSystem
+{
+    //Case（ケース）クラス
+    public class Case
+    {
+        //ケース番号
+        public string CaseNumber { get; set; }
+
+        //クライアント整理番号
+        public string ClientReference { get; set; }
+
+        //クライアント名
+        public string ClientName { get; set; }
+
+        //窓口担当者
+        public string Contact { get; set; }
+
+        //担当者
+        public string Engineer { get; set; }
+
+        //クライアント知財担当者
+        public string ClientContact { get; set; }
+
+        //発明者
+        public string Inventor { get; set; }
+
+        //期限管理フラグ
+        public string Flag { get; set; }
+
+        //カテゴリ
+        public string Category { get; set; }
+
+        //地域
+        public string Region { get; set; }
+
+        //期限
+        public string DueDate { get; set; }
+
+        //スレッドリンク
+        public string Thread { get; set; }
+
+        //Wrapperリンク
+        public string Wrapper { get; set; }
+
+        //Roosterリンク
+        public string Rooster { get; set; }
+
+        //受託日
+        public string CaseReceived { get; set; }
+
+        //面談日
+        public string Meeting { get; set; }
+
+        //補充資料受領日
+        public string SupplementReceived { get; set; }
+
+        //ドラフト期限
+        public string DraftDeadline { get; set; }
+
+        //ドラフト送付日
+        public string DraftSent { get; set; }
+    }
+}
---
+++ ProgressManagementSystem/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressManagementSystem
+{
+    //Client（クライアント）クラス
+    public class Client
+    {
+        //クライアント名
+        public string Name { get; set; }
+
+        //クライアント情報（リンク）
+        public string Information { get; set; }
+    }
+}
